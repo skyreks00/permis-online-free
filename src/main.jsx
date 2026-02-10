ecddc6cb8b294067fb9676a7d15d2bb3bc271e13
sunshine <sunshine.wilmus.mail@gmail.com>
2026-02-10 14:31:32 +0100

    fix: correct css import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './assets/styles.css'
 
 console.log("APP VERSION: 2026-02-10 14:30 - Fixed single_choice & Logic");
 
