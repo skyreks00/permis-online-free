9296bcc2c847f5e2884550b8428a9268d54ce048
dstl4 <141284777+stotwo@users.noreply.github.com>
2026-02-17 14:43:04 +0100

    fix api key

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GithubAuthProvider, signInWithPopup, signOut } from "firebase/
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAGA-3ns3qFV47KXsFfd97kac_9LhPyhY",
-  authDomain: "permis-online-sync.firebaseapp.com",
-  projectId: "permis-online-sync",
-  storageBucket: "permis-online-sync.firebasestorage.app",
-  messagingSenderId: "653028638132",
-  appId: "1:653028638132:web:028b727230365733a846e1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
